6c974ed0cbca85396f9d8f9a17403014362fe436
sakria9 <i@sustech.moe>
2024-09-23 16:44:15 +0800

    Add project files.

---
+++ supersonic/supersonic.cpp
@@ -0,0 +1,12 @@
+﻿// supersonic.cpp : Defines the entry point for the application.
+//
+
+#include "supersonic.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
